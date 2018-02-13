a46128ceeeb954902205131a7dabe57c5c4cc806
Bowen Zhong <bz4e14@soton.ac.uk>
2018-02-13 18:42:54 +0800

    pagnation and sort

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.http import JsonResponse
 from django.core import serializers
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 
 from .models import CourseOrg, CityDict
 
@@ -15,11 +17,41 @@ class OrgView(View):
 
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
-        org_nums = all_orgs.count()
+        hot_orgs = all_orgs.order_by("-click_nums")[:3]
         all_cities = CityDict.objects.all()
+
+        city_id = request.GET.get('city', '')
+        if city_id:
+            all_orgs = all_orgs.filter(city_id = int(city_id))
+
+        category = request.GET.get('category', "")
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        sort = request.GET.get('sort', "")
+        if sort:
+            if sort == 'students':
+                all_orgs = all_orgs.order_by("-students")
+            elif sort == 'courses':
+                all_orgs = all_orgs.order_by("-course_nums")
+
+        org_nums = all_orgs.count()
+
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        # Provide Paginator with the request object for complete querystring generation
+        p = Paginator(all_orgs, 5, request=request)
+
+        orgs = p.page(page).object_list
         data = {
-            'allOrgs': serializers.serialize('json', all_orgs),
+            'allOrgs': serializers.serialize('json', orgs),
             'allCities': serializers.serialize('json', all_cities),
-            'org_nums': org_nums
+            'org_nums': org_nums,
+            'city_id': city_id,
+            'category': category,
+            'hot_orgs': serializers.serialize('json', hot_orgs)
         }
         return JsonResponse(data)
